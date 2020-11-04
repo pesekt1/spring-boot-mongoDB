ea7110cd453aba09b92d935c3a9737b928b36b36
pesekt1 <58634005+pesekt1@users.noreply.github.com>
2020-11-04 16:23:19 +0100

    -

---
+++ src/main/java/swc3/mongodbwebserver/controller/HelloController.java
@@ -0,0 +1,16 @@
+package swc3.mongodbwebserver.controller;//controller used in the beginning of the project to make sure it works.
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
